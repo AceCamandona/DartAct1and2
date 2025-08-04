9788cdefd20a46333e9abf4c6f3148897a57ce21
AceCamandona <allyssagracecamandona@gmail.com>
2025-08-04 13:09:05 +0800

    Activity 1 and 2 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,11 @@
+void main(){
+ var name = "Alice";
+  print("Welcome $name");
+  
+  String newGuest= "Alice";
+  print("Welcome, $newGuest!");
+  
+  dynamic guestName = "Alice";
+  guestName = 25;
+  print("Welcome, $guestName");
+}
